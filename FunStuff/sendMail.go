9c2e3f56cd23c8d91e33a0b7062345acfa578654
prakhar <pmprakhargenius@gmail.com>
2016-10-16 18:57:00 +0530

    evaluated gomail

---
+++ FunStuff/sendMail.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	   "fmt"
+	   "gopkg.in/gomail.v2"
+           "os"
+	   "log"   
+       )
+
+
+func check(e error) {
+ 
+    if e != nil {
+        log.Fatal(e)
+    }
+}
+
+func main() {
+ 
+    arguments := os.Args[1:] 
+    email := arguments[0]
+    passwd := arguments[1]
+    to := arguments[2]
+
+    mail := gomail.NewMessage()
+    mail.SetHeader("From", email)
+    mail.SetHeader("To", to)
+    mail.SetHeader("Subject", "Test GO Mail")
+    mail.SetBody("text/plain", "Hello, How are you!")
+    //mail.Attach("")
+
+    dialer := gomail.NewDialer("smtp.gmail.com", 587, email, passwd)
+    
+    check(dialer.DialAndSend(mail))
+    fmt.Println("Mail Send")
+}
